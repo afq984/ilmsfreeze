9a01892582276985e5cddbc0eba74987f2425f5a
Li-Yu Yu <afg984@gmail.com>
2021-07-09 09:12:48 +0800

    refactor renderTable into <freeze-table>

--- freeze-app/src/freeze-browse.ts
+++ freeze-app/src/freeze-browse.ts
@@ -2,29 +2,14 @@ import { html } from "lit";
 import { customElement } from "lit/decorators.js";
 
 import { BaseView } from "./base-view.js";
+import "./freeze-table";
 
 @customElement("freeze-browse")
 export class FreezeBrowse extends BaseView {
-  renderTable(items: Array<any>, attrs: Array<string>) {
-    return html`
-    <table class="table">
-      <tr>
-      ${attrs.map((attr) => html`<th>${attr}</th>`)}
-  </tr>
-  </tr>
-      ${items.map(
-        (item) =>
-          html`<tr>
-            ${attrs.map((attr) => html`<td>${item[attr]}</td>`)}
-          </tr>`
-      )}
-      </table>
-    `;
-  }
-
   render() {
+    const fields = ["id", "serial", "is_admin", "name"];
     return html` <div class="container">
-      ${this.renderTable(this.courses, ["id", "serial", "is_admin", "name"])}
+      <freeze-table .items=${this.courses} .fields=${fields}></freeze-table>
     </div>`;
   }
 
