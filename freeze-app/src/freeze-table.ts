b8133e6e5785d391e9c56f436ef55901b1ccfbc4
Li-Yu Yu <afg984@gmail.com>
2021-07-09 16:35:46 +0800

    make <freeze-table> sortable

--- freeze-app/src/freeze-table.ts
+++ freeze-app/src/freeze-table.ts
@@ -1,6 +1,9 @@
 import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
+const symbolAscending = "⬆︎";
+const symbolDescending = "⬇︎";
+
 @customElement("freeze-table")
 export class FreezeTable extends LitElement {
   createRenderRoot() {
@@ -13,17 +16,67 @@ export class FreezeTable extends LitElement {
   @property({ attribute: false })
   fields: Array<any>;
 
+  @property()
+  sortField?: string;
+
+  @property({ type: Boolean })
+  sortDescending: boolean = false;
+
   constructor() {
     super();
     this.items = [];
     this.fields = [];
   }
 
+  connectedCallback() {
+    super.connectedCallback();
+    if (this.sortField !== undefined) {
+      this.sort(this.sortField, this.sortDescending);
+    }
+  }
+
+  sort() {
+    if (this.sortField === undefined) {
+      return;
+    }
+    const m = this.sortDescending ? -1 : 1;
+    const field = this.sortField;
+    this.items.sort(
+      (a, b) =>
+        ((a[field] > b[field] ? 1 : 0) - (a[field] < b[field] ? 1 : 0)) * m
+    );
+  }
+
+  toggleSort(field: string) {
+    if (this.sortField === field) {
+      this.sortDescending = !this.sortDescending;
+    } else {
+      this.sortField = field;
+      this.sortDescending = false;
+    }
+  }
+
   render() {
+    if (this.sortField !== undefined) {
+      this.sort();
+    }
     return html`
     <table class="table">
       <tr>
-      ${this.fields.map((field) => html`<th>${field}</th>`)}
+      ${this.fields.map(
+        (field) =>
+          html`<th
+            style="cursor: pointer; user-select: none"
+            @click=${() => this.toggleSort(field)}
+          >
+            ${field}
+            ${field === this.sortField
+              ? this.sortDescending
+                ? symbolDescending
+                : symbolAscending
+              : ""}
+          </th>`
+      )}
   </tr>
   </tr>
       ${this.items.map(
