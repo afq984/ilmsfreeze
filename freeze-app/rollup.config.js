5fce32f01c32da84a333f1778bf1e3ad8096c1f2
Yu, Li-Yu <afg984@gmail.com>
2021-07-11 02:11:16 +0800

    add static assets

--- freeze-app/rollup.config.js
+++ freeze-app/rollup.config.js
@@ -1,6 +1,6 @@
 // Import rollup plugins
 import html from "@web/rollup-plugin-html";
-// import {copy} from '@web/rollup-plugin-copy';
+import { copy } from "@web/rollup-plugin-copy";
 import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import minifyHTML from "rollup-plugin-minify-html-literals";
@@ -26,9 +26,9 @@ export default {
     // Print bundle summary
     summary(),
     // Optional: copy any static assets to build directory
-    // copy({
-    //   patterns: ['images/**/*'],
-    // }),
+    copy({
+      patterns: ["sys/**/*"],
+    }),
   ],
   output: {
     dir: "out/dist",
