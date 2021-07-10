8d10a629ccb8a66e15568f215de808af9d4c7de3
Yu, Li-Yu <afg984@gmail.com>
2021-07-10 16:57:09 +0800

    rename Course => CourseMeta

--- freeze-app/src/freeze-course.ts
+++ freeze-app/src/freeze-course.ts
@@ -2,7 +2,7 @@ import { html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { unsafeHTML } from "lit/directives/unsafe-html.js";
 
-import { BaseView, Course } from "./base-view.js";
+import { BaseView, CourseMeta } from "./base-view.js";
 import { FileSystemDataSource } from "./data-source.js";
 import { Fragment, homeFragment } from "./freeze-pathbar.js";
 import "./freeze-sidemenu";
@@ -13,7 +13,7 @@ export class FreezeCourse extends BaseView {
   course_id?: number;
 
   @property({ attribute: false })
-  course?: Course;
+  courseMeta?: CourseMeta;
 
   @property({ attribute: false })
   body = "";
@@ -38,12 +38,12 @@ export class FreezeCourse extends BaseView {
       return;
     }
     const source = new FileSystemDataSource(rootHandle);
-    this.course = await source.getMeta("course", this.course_id);
+    this.courseMeta = await source.getMeta("course", this.course_id);
     this.body = await source.getText("course", this.course_id, "index.html");
     this.fragments = [
       homeFragment,
       {
-        text: (this.course as Course).name,
+        text: this.courseMeta!.name,
         href: `/course/${this.course_id}`,
         active: true,
       },
@@ -56,7 +56,7 @@ export class FreezeCourse extends BaseView {
       <div class="columns">
         <freeze-sidemenu
           class="column is-one-fifth"
-          .course=${this.course}
+          .courseMeta=${this.courseMeta}
         ></freeze-sidemenu>
         <div class="column">
           <div class="content">${unsafeHTML(this.body)}</div>
