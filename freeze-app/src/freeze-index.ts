ca724bef669f5922fdebdd949b223f55eaaa01ff
Yu, Li-Yu <afg984@gmail.com>
2021-07-13 01:16:59 +0800

    add service worker detection

--- freeze-app/src/freeze-index.ts
+++ freeze-app/src/freeze-index.ts
@@ -1,5 +1,5 @@
 import { html, LitElement, TemplateResult } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, state } from "lit/decorators.js";
 import { externalLink, materialIcon } from "./html";
 
 type statusClass = {
@@ -27,6 +27,35 @@ export class FreezeIndex extends LitElement {
   createRenderRoot() {
     return this;
   }
+  @state()
+  fsAvailable = "showDirectoryPicker" in window;
+  @state()
+  serviceWorkerStatus = statusWarn;
+  @state()
+  serviceWorkerDescription = "Unknown";
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.checkServiceWorkerStatus();
+  }
+
+  async checkServiceWorkerStatus() {
+    const response = await fetch("/sw-status.json");
+    const status = await response.json();
+    switch (status.version) {
+      case null:
+        this.serviceWorkerStatus = statusWarn;
+        this.serviceWorkerDescription = "Unavailable";
+        break;
+      case 1:
+        this.serviceWorkerStatus = statusSuccess;
+        this.serviceWorkerDescription = "Active";
+        break;
+      default:
+        this.serviceWorkerStatus = statusWarn;
+        this.serviceWorkerDescription = "Unknown";
+    }
+  }
 
   renderStatus(cls: statusClass, text: TemplateResult | string) {
     return html`<span class="icon-text">
@@ -36,20 +65,19 @@ export class FreezeIndex extends LitElement {
   }
 
   render() {
-    const fsAvailable = "showDirectoryPicker" in window;
     return html`
       <h1 class="title">Browser status</h1>
       <div class="content">
         <dl>
           <dt>
             <strong>File System Access API</strong>
-            ${fsAvailable
+            ${this.fsAvailable
               ? this.renderStatus(statusSuccess, "Available")
-              : this.renderStatus(statusFail, html`Unsupported`)}
+              : this.renderStatus(statusFail, "Unsupported")}
           </dt>
           <dd>
-            You must use a browser with this feature to load/save files from/to
-            your disk.<br />
+            <em>Required</em> to load/save backup iLMS data from/to your
+            disk.<br />
             See
             ${externalLink(
               "supported browsers",
@@ -57,11 +85,24 @@ export class FreezeIndex extends LitElement {
             )}.
           </dd>
           <dt>
+            <strong>Service Worker</strong>
+            ${this.renderStatus(
+              this.serviceWorkerStatus,
+              this.serviceWorkerDescription
+            )}
+          </dt>
+          <dd>
+            The service worker provides access to
+            <em>attachments, videos, and inline images</em>.<br />
+            Try close all tabs and reload this site if there is a problem.
+          </dd>
+          <dt>
             <strong>Chrome Extension</strong>
             ${this.renderStatus(statusWarn, "Not Implemented")}
           </dt>
           <dd>
-            You need the Chrome Extension to download from the browser.<br />
+            You need the Chrome Extension to <em>download</em> from the
+            browser.<br />
             You can also load the <code>ilmsdump.out</code> directory produced
             by
             ${externalLink("ilmsdump", "https://github.com/afq984/ilmsdump")}.
