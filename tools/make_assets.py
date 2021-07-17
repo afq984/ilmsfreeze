ae28c72bc34aa08a5fb414b1611766e7dd3c4c70
Yu, Li-Yu <afg984@gmail.com>
2021-07-17 14:57:13 +0800

    optimize icons

--- tools/make_assets.py
+++ tools/make_assets.py
@@ -1,6 +1,9 @@
 import subprocess
 import glob
 import os
+import tempfile
+
+import lxml.etree
 
 
 ICON_MAPPING = {
@@ -33,5 +36,26 @@ os.makedirs(OUTDIR, exist_ok=True)
 for asset, icon in ICON_MAPPING.items():
     dst = os.path.join(OUTDIR, asset)
     print(dst)
-    src, = glob.glob(f'material-design-icons/src/*/{icon}/materialicons/24px.svg')
-    subprocess.check_call(['convert', src, dst])
+    (src,) = glob.glob(f'material-design-icons/src/*/{icon}/materialicons/24px.svg')
+    svg = lxml.etree.parse(src)
+    for g in svg.xpath('//*[local-name()="path"]'):
+        if g.attrib.get('fill') == 'none':
+            continue
+        g.attrib['style'] = 'fill: #4a4a4a'
+    with tempfile.TemporaryDirectory() as d:
+        png = subprocess.run(
+            [
+                'inkscape',
+                '--pipe',
+                f'--export-filename={d}/out.png',
+                '--export-type=png',
+                '--export-width=16',
+                '--export-height=16',
+            ],
+            input=lxml.etree.tostring(svg),
+            check=True,
+        )
+        subprocess.run(
+            ['convert', f'{d}/out.png', dst],
+            check=True,
+        )
