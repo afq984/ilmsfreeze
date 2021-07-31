af201807cf2aff14f3d9f151b9f3d3d605d4cadc
Li-Yu Yu <afg984@gmail.com>
2021-07-31 10:10:44 +0800

    always visible navbar
    fixes #27

--- freeze-app/src/freeze-navbar.ts
+++ freeze-app/src/freeze-navbar.ts
@@ -31,7 +31,7 @@ export class FreezeNavbar extends DirectoryChangeAwareView {
     }
   }
 
-  renderDirectoryStatus() {
+  private renderDirectoryStatus() {
     const directoryAccessClasses = {
       tag: true,
       "is-success": this.directoryAccess === "readwrite",
@@ -56,54 +56,40 @@ export class FreezeNavbar extends DirectoryChangeAwareView {
       role="navigation"
       aria-label="main navigation"
     >
-      <section class="container">
-        <div class="navbar-brand">
+      <section class="container" style="display: flex">
+        <div
+          style="display: flex; justify-content: flex-start; align-items: center;"
+        >
           <a class="navbar-item" href="/">
             <strong>ilmsfreeze</strong>
           </a>
-
-          <a
-            role="button"
-            class="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
+          <a href="/course/" class="navbar-item">Browse</a>
+          <a href="/download" class="navbar-item">Download</a>
         </div>
 
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-            <a href="/course/" class="navbar-item">Browse</a>
-            <a href="/download" class="navbar-item">Download</a>
-          </div>
-
-          <div class="navbar-end">
-            <div class="navbar-item">${this.renderDirectoryStatus()}</div>
-            <div class="navbar-item">
-              <div class="buttons">
-                <button
-                  class="button is-primary"
-                  @click="${this._dispatchClick}"
-                >
-                  <strong class="icon-text"
-                    >${materialIcon("folder")}<span>Open...</span></strong
-                  >
-                </button>
-              </div>
-            </div>
-          </div>
+        <div
+          class="navbar-end"
+          style="display: flex; justify-content: flex-end; align-items: center;"
+        >
+          <div class="navbar-item">${this.renderDirectoryStatus()}</div>
+          <div class="navbar-item">${this.renderOpenButton()}</div>
         </div>
       </section>
     </nav>`;
   }
 
+  private renderOpenButton() {
+    return html`<div class="buttons">
+      <button class="button is-primary" @click="${this._dispatchClick}">
+        <strong class="icon-text"
+          >${materialIcon("folder")}<span>Open...</span></strong
+        >
+      </button>
+    </div>`;
+  }
+
   private _dispatchClick() {
     const options = {
-      detail: "yee",
       bubbles: true,
       composed: true,
     };
